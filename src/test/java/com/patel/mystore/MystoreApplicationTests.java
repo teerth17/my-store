916f2d1dbcfd2c362032471dd1a778ce3d1e1a67
Teerth Shailesh Patel <teerthshailesh.patel@scranton.edu>
2024-09-26 00:02:16 -0400

    initial

---
+++ src/test/java/com/patel/mystore/MystoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patel.mystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
